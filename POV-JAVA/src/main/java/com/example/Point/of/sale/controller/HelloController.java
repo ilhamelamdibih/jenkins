7a8bdbf73c4538b94e43d55df837eed56b0282d6
ilhamelamdibih <ilhame.lamdibih@gmail.com>
2024-01-11 23:28:51 +0000

    change controller hello test

--- POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello from new test from ilhame hahahaha :)";
+        return "Hello from new test from ilhame other test :)";
     }
 
     @GetMapping("/user")
